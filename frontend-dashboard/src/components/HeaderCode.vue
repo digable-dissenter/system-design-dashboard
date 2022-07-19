3da3e85ea3848f8d0393e1d1d8819b14426f5b38
kennethss <kenneth.ssekimpi@allangray.co.za>
2022-07-19 17:30:33 +0200

    Neatened up code a bit

--- frontend-dashboard/src/components/HeaderCode.vue
+++ frontend-dashboard/src/components/HeaderCode.vue
@@ -1,37 +1,30 @@
 <template>
-<div class="nav">
-<a href="#">Home</a>
-<a href="#">Static Reports</a>
-<a v-on:click="logout" href="#">Logout</a>
-
+  <div class="nav">
+  <a href="#">Home</a>
+  <a href="#">Static Reports</a>
+  <a v-on:click="logout" href="#">Logout</a>
 </div>
 </template>
 
 <script>
 
-
-export default {
-
-    name: 'HeaderCode',
-
-    methods:{
-        logout(){
-        localStorage.clear();
-        this.$router.push({name:'LoginPage'})
-        }
-
-        
-    }
-}
+  export default {
+      name: 'HeaderCode',
+      methods:{
+          logout(){
+          localStorage.clear();
+          this.$router.push({name:'LoginPage'})
+          }          
+      }
+  }
 
 </script>
 
 <style>
 
-.nav{
-  background-color:#1E71BB;
-  overflow : hidden
-}
-
+  .nav{
+    background-color:#1E71BB;
+    overflow : hidden
+  }
 
 </style>
