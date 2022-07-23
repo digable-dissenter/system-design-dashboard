e2543a771b9eb1dd549ae581af77e6ab4103d6c6
digable-dissenter <this_is_skimps@hotmail.co.uk>
2022-07-23 11:36:57 +0200

    latest copy

--- frontend-dashboard/src/main.js
+++ frontend-dashboard/src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './routers';
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap.css'
+Vue.use(BootstrapVue)
 createApp(App)
 .use(router)
 .mount('#app');
