4c5af2f7b14c4ffa14caba288d39a12a55f3c5a9
Kenneth Ssekimpi <this_is_skimps@hotmail.co.uk>
2022-08-04 14:55:50 +0200

    merge conflict

--- backend/app/dataframes.py
+++ backend/app/dataframes.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-from app import main
-import pandas as pd
-
-tbl_Industry_Classification_Benchmark = main.frames_dict["dbo_tbl_Industry_Classification_Benchmark"]
-tbl_FTSEJSE_Index_Series = main.frames_dict["dbo_tbl_FTSEJSE_Index_Series"]
-tbl_EOD_Equity_Data = main.frames_dict["dbo_tbl_EOD_Equity_Data"]
-tbl_EOD_Interest_Rate_Data = main.frames_dict["dbo_tbl_EOD_Interest_Rate_Data"]
-tbl_Beta_Output = main.frames_dict["dbo_tbl_Beta_Output"]
-tbl_BA_Beta_Output = main.frames_dict["dbo_tbl_BA_Beta_Output"]
-tbl_Index_Constituents = main.frames_dict["dbo_tbl_Index_Constituents"]
-=======
 '''
     Author: Kenneth Ssekimpi
 '''
