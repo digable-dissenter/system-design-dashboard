1e162e84160c1195e67fe6446f2ae94fb8c2bf88
Daiyaan-Salie <SLXABD007@myuct.ac.za>
2022-08-04 14:56:28 +0200

    dataframes.py correction

--- backend/app/dataframes.py
+++ backend/app/dataframes.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from app import main
 import pandas as pd
 
-tbl_Industry_Classification_Benchmark = main.frames_dict["dbo_tbl_Industry_Classification_Benchmark"]
-tbl_FTSEJSE_Index_Series = main.frames_dict["dbo_tbl_FTSEJSE_Index_Series"]
-tbl_EOD_Equity_Data = main.frames_dict["dbo_tbl_EOD_Equity_Data"]
-tbl_EOD_Interest_Rate_Data = main.frames_dict["dbo_tbl_EOD_Interest_Rate_Data"]
-tbl_Beta_Output = main.frames_dict["dbo_tbl_Beta_Output"]
-tbl_BA_Beta_Output = main.frames_dict["dbo_tbl_BA_Beta_Output"]
-tbl_Index_Constituents = main.frames_dict["dbo_tbl_Index_Constituents"]
-=======
 '''
     Author: Kenneth Ssekimpi
 '''
@@ -34,4 +25,3 @@ df_Index_Constituents = df_Index_Constituents.set_index("Alpha")
 df_Industry_Classification_Benchmark = df_Industry_Classification_Benchmark.set_index("Sub-Sector")
 
 from app import indices, sectors, shares
->>>>>>> 40c1656f989b06907c813790f61474f85ce1b478
